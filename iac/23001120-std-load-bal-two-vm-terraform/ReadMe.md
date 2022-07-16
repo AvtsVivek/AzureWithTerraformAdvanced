077c0a8746a2b9827cc6dca55d71a2d038127c4c
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-07-16 19:23:43 +0530

    Added new ex

---
+++ iac/23001120-std-load-bal-two-vm-terraform/ReadMe.md
@@ -0,0 +1,13 @@
+# Standard Load Balancer with Terraform
+
+- This example is step towards buiding the load balancer example built earlier manually. But using terraform.
+
+- This example is built on top of the [`iac-23001100-std-load-bal-manual`](https://github.com/AvtsVivek/AzureWithTerraformAdvanced/tree/main/iac/23001000-std-load-bal-manual) example.
+
+- In this example, resource group, vnet along with subnet, and vm are created using terraform.
+
+- Standard load balancer is added in a subsequent exercise.
+
+- What is achieved is the following.
+
+![Vnet topology to be created](./topology.svg)
---
+++ iac/23001120-std-load-bal-two-vm-terraform/commands.sh
@@ -0,0 +1,41 @@
+
+# cd into the directory.
+cd ./iac/23001120-std-load-bal-two-vm-terraform
+
+cd ../..
+
+terraform fmt
+
+terraform init
+
+terraform validate
+
+terraform plan -out main.tfplan
+
+terraform show main.tfplan
+
+terraform apply main.tfplan
+
+# Once successfully applied, Review the resources.
+# Download the topology diagram. Go to the created vnet and then diagram.
+# Also look at subnets and corresponding security groups.
+
+terraform state list
+
+# To create diagram, run the following command.
+code . -r 
+# Ensure Azure Terraform extension is installed in VS Code
+# Press F1 and Run the command AzureTerraform: Visualize. Graph.png will be generated.
+
+# For the following command to work, you need to pass on the resource, or data source.
+# This resource or data source is got from terraform state list command
+terraform state show 
+
+terraform show terraform.tfstate
+
+terraform plan -destroy -out main.destroy.tfplan
+
+terraform show main.destroy.tfplan
+
+terraform apply main.destroy.tfplan
+
