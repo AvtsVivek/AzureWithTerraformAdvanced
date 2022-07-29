bf41f143c39b58150bf904d1a21cab3ef4f257df
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-07-29 14:52:12 +0530

    Added crt etc to git ignore file

--- iac/33001000-az-app-gateway-ssl-https-redirect-error-pages/commands-ssl-generate.sh
+++ iac/33001000-az-app-gateway-ssl-https-redirect-error-pages/commands-ssl-generate.sh
@@ -7,6 +7,9 @@ cd ./iac/33001000-az-app-gateway-ssl-https-redirect-error-pages
 
 cd ssl-self-signed
 
+# Run the following command to generate a self-signed certificate for the app gateway.
+# Run them using git bash and NOT POWERSHELL.
+
 openssl req -newkey rsa:2048 -nodes -keyout httpd.key -x509 -days 7300 -out httpd.crt
 
 # Noe create the pfx file.
